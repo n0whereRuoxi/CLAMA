85aa89bea4af07130dd12a40718a6768250f4ee1
n0whereRuoxi <lrxblly@gmail.com>
2025-02-23 07:17:47 -0500

    instructions

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
-GCC 8.5.0 to compile HTNMakerC
+GCC 8.5.0 should work for compiling HTNMakerC
 Python 3.10.10 to run CurricuLAMA/train.py
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install dependencies from requirements.txt
+# Python 3.10.10 works without any dependency conflict
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
@@ -13,6 +14,13 @@ rm -rf builds/release/CMakeFiles
 ./build.py release
 
 # Build HTN-Maker-C
+# GCC 8.5.0 should work for compiling HTNMakerC
 cd ../HTNMakerC
 ./configure
 make
+
+# Try first experiment 
+cd ../CurricuLAMA
+python3 train.py --debug 2 blocks
+# The learned HTN methods are in Curriculama/experiments/blocks/*.pddl
+# The digit in *.pddl correspond to the problem number
